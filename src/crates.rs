4cb5d5366b6a5e669ccfd95b22957d8c57b952b1
Chris MacNaughton <chris.macnaughton@canonical.com>
2020-02-29 11:27:25 +0100

    Update to new crate path
    This change also runs rustfmt on the crates.rs file

--- src/crates.rs
+++ src/crates.rs
@@ -1,13 +1,13 @@
 use std::fs::{self, File};
 // use std::fs::File;
-use std::io::prelude::*;
 use std::io;
+use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::{Command, ExitStatus};
 use std::thread;
 
-use serde_json;
 use scoped_threadpool::Pool;
+use serde_json;
 use walkdir::WalkDir;
 
 use super::Config;
@@ -18,45 +18,56 @@ pub struct Package {
     vers: String,
 }
 
-pub fn fetch(path: &PathBuf,
-             upstream: &str,
-             index_path: &str,
-             crate_name: &str,
-             crate_version: &str)
-             -> Result<ExitStatus, io::Error> {
+pub fn fetch(
+    path: &PathBuf,
+    upstream: &str,
+    index_path: &str,
+    crate_name: &str,
+    crate_version: &str,
+) -> Result<ExitStatus, io::Error> {
     debug!("Fetching {}(v: {})", crate_name, crate_version);
-    let url = format!("{}{}/{}/download", upstream, crate_name, crate_version);
+    let url = format!(
+        "{}{}/{}-{}.crate",
+        upstream, crate_name, crate_name, crate_version
+    );
     trace!("Fetching from {}", url);
-    let _ = fs::create_dir_all(PathBuf::from(format!("{}/crates/{}", index_path, crate_name)));
-    Command::new("curl").arg("-o").arg(&path) // Save to disk
-                         .arg("-L") // Follow redirects
-                         .arg("-s") // Quietly!
-                         .arg(url)
-                         .status()
+    let _ = fs::create_dir_all(PathBuf::from(format!(
+        "{}/crates/{}",
+        index_path, crate_name
+    )));
+    Command::new("curl")
+        .arg("-o")
+        .arg(&path) // Save to disk
+        .arg("-L") // Follow redirects
+        .arg("-s") // Quietly!
+        .arg(url)
+        .status()
 }
 
 pub fn size(path: &PathBuf) -> u64 {
-  match fs::metadata(path) {
-    Ok(metadata) => metadata.len(),
-    _ => 0
-  }
+    match fs::metadata(path) {
+        Ok(metadata) => metadata.len(),
+        _ => 0,
+    }
 }
 
 fn try_fetch(config: &Config, crate_name: &str, crate_version: &str) {
-    let path = PathBuf::from(format!("{}/crates/{}/{}",
-                                     config.index_path,
-                                     crate_name,
-                                     crate_version));
+    let path = PathBuf::from(format!(
+        "{}/crates/{}/{}",
+        config.index_path, crate_name, crate_version
+    ));
     if path.exists() {
         trace!("{}:{} is already fetched", crate_name, crate_version);
     } else {
-        match fetch(&path,
-                      &config.upstream,
-                      &config.index_path,
-                      &crate_name,
-                      &crate_version) {
-          Ok(_) => {},
-          Err(e) => error!("Couldn't fetch {}/{}: {:?}", crate_name, crate_version, e),
+        match fetch(
+            &path,
+            &config.upstream,
+            &config.index_path,
+            &crate_name,
+            &crate_version,
+        ) {
+            Ok(_) => {}
+            Err(e) => error!("Couldn't fetch {}/{}: {:?}", crate_name, crate_version, e),
         }
     }
 }
@@ -83,7 +94,6 @@ pub fn pre_fetch(config: &Config) {
     }
 }
 
-
 pub fn fetch_all(config: &Config) {
     if !config.all {
         return;
@@ -100,7 +110,8 @@ pub fn fetch_all(config: &Config) {
                 .filter_map(|e| e.ok())
                 .filter(|f| !f.path().to_str().unwrap().contains(".git"))
                 .filter(|f| f.file_type().is_file())
-                .filter(|f| f.file_name() != "config.json") {
+                .filter(|f| f.file_name() != "config.json")
+            {
                 trace!("Found crate info file at {:?}", entry.path());
 
                 let config = config.clone();
@@ -109,26 +120,23 @@ pub fn fetch_all(config: &Config) {
                         let reader = io::BufReader::new(f);
                         for line in reader.lines().filter_map(|l| l.ok()) {
                             match serde_json::from_str::<Package>(&line) {
-                            // match json::decode::<Package>(&line) {
+                                // match json::decode::<Package>(&line) {
                                 Ok(package) => {
                                     trace!("Found package: {:?}", package);
 
                                     try_fetch(&config, &package.name, &package.vers);
                                 }
-                                Err(e) => {
-                                    warn!("Had a problem with \"{}\" / {:?}: {:?}",
-                                          line,
-                                          entry.path(),
-                                          e)
-                                }
+                                Err(e) => warn!(
+                                    "Had a problem with \"{}\" / {:?}: {:?}",
+                                    line,
+                                    entry.path(),
+                                    e
+                                ),
                             };
-
                         }
-
                     }
                 });
             }
-
         });
 
         debug!("Finished background fetch all");
